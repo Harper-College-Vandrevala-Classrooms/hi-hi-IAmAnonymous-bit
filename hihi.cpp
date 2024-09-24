ac792768f9cb7c09c50c12aa5056493191d27974
NNR8608220 <n8reyes2006@gmail.com>
2024-09-24 13:28:54 -0500

    Function with ending arguement

--- hihi.cpp
+++ hihi.cpp
@@ -34,6 +34,35 @@ using namespace std
 
 }
 
+;int strcmp_case_insensitive(string str1, string str2, int stop)
+{
+
+    int ascii1 = 0;
+    int ascii2 = 0;
+    
+    for (int i = 0; i < stop; i++)
+    {
+        char ch1 = tolower(str1[i]);
+        if (ch1 != ' ')
+        {
+            ascii1 += ch1;
+        }
+    }
+
+    for (int i = 0; i < stop; i++)
+    {
+        char ch2 = tolower(str2[i]);
+        if (ch2 != ' ')
+        {
+            ascii2 += ch2;
+        }
+        
+    }
+
+    return ascii1 - ascii2;
+
+}
+
 int main()
 {
     assert(strcmp_case_insensitive("hi", "hi") == 0);
@@ -43,6 +72,10 @@ int main()
     assert(strcmp_case_insensitive("", "hi" ) < 0);
     assert(strcmp_case_insensitive("hil", "hi") > 0);
     assert(strcmp_case_insensitive("h i", "hi") == 0);
+
+    assert(strcmp_case_insensitive("Stars", "Start", 4) == 0);
+    assert(strcmp_case_insensitive("Starch", "Starts", 5) < 0);
+    assert(strcmp_case_insensitive("Starts", "Starch", 5) > 0);
 
     cout << "All code works well!" << "\n";
 
